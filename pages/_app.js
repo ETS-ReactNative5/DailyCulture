b34414d824f1db64bfefdafdfb78e65cc7875142
Chris Boyle <chris.j.boyle88@gmail.com>
2021-06-19 21:51:06 -0500

    updated ignore and fixed code issue with useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -47,22 +47,7 @@ Router.events.on('routeChangeError', () => {
 });
 
 export default class MyApp extends App {
-  componentDidMount() {
-    useEffect(() => {
-      const handleRouteChange = (url) => {
-        ga.pageview(url);
-      };
-      //When the component is mounted, subscribe to router changes
-      //and log those page views
-      router.events.on('routeChangeComplete', handleRouteChange);
-
-      // If the component is unmounted, unsubscribe
-      // from the event with the `off` method
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange);
-      };
-    }, [router.events]);
-  }
+  componentDidMount() {}
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
 
